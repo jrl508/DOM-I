8cc376d4488ddbe6b00232d2630d444ca078c46e
jrl508 <jrl47@yahoo.com>
2019-05-13 19:18:22 -0400

    WIP initial commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,43 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let links = document.querySelectorAll('a')
+
+navItem1 = links[0]
+navItem1.textContent = siteContent["nav"]["nav-item-1"]
+navItem2 = links[1]
+navItem2.textContent = siteContent["nav"]["nav-item-2"]
+navItem3 = links[2]
+navItem3.textContent = siteContent["nav"]["nav-item-3"]
+navItem4 = links[3]
+navItem4.textContent = siteContent["nav"]["nav-item-4"]
+navItem5 = links[4]
+navItem5.textContent = siteContent["nav"]["nav-item-5"]
+navItem6 = links[5]
+navItem6.textContent = siteContent["nav"]["nav-item-6"]
+
+ctaH1 = document.querySelector('.cta-text > h1')
+ctaH1.textContent = siteContent["cta"]["h1"]
+ctaImg = document.getElementById("cta-img")
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+ctaBtn = document.querySelector('button')
+ctaBtn.textContent = siteContent['cta']["button"]
+
+topContent = document.querySelectorAll('.top-content > .text-content')
+topContent[0].querySelector('h4').textContent = siteContent["main-content"]["features-h4"]
+topContent[1].querySelector('h4').textContent = siteContent["main-content"]["about-h4"]
+topContent[0].querySelector('p').textContent = siteContent["main-content"]["features-content"]
+topContent[1].querySelector('p').textContent = siteContent["main-content"]["about-content"]
+
+midImg = document.getElementById("middle-img")
+midImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+bottomContent = document.querySelectorAll('.bottom-content > .text-content')
+bottomContent[0].querySelector('h4').textContent = siteContent["main-content"]['services-h4']
+bottomContent[1].querySelector('h4').textContent = siteContent["main-content"]['product-h4']
+bottomContent[2].querySelector('h4').textContent = siteContent["main-content"]['vision-h4']
+bottomContent[0].querySelector('p').textContent = siteContent["main-content"]['services-content']
+bottomContent[1].querySelector('p').textContent = siteContent["main-content"]['product-content']
+bottomContent[2].querySelector('p').textContent = siteContent["main-content"]['vision-content']
+
